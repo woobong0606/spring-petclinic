83ff8f6931aabca1aec851a2d6b8f2227648decf
sjh4616 <41767000+sjh4616@users.noreply.github.com>
2024-03-02 12:28:42 +0900

    Create run_process.sh

---
+++ deploy/scripts/run_process.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REGION="ap-northeast-2"
+DOCKER_IMAGE_NAME="aws00-spring-petclinic"        
+ECR_REPOSITORY="257307634175.dkr.ecr.ap-northeast-2.amazonaws.com"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/${DOCKER_IMAGE_NAME}"
+ECR_DOCKER_TAG="latest"
+
+aws ecr get-login-password --region ${REGION} \
+  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
+
+export IMAGE=${ECR_DOCKER_IMAGE};
+export TAG=${ECR_DOCKER_TAG};
+docker-compose -f /home/ubuntu/docker-compose.yml up -d --build;
